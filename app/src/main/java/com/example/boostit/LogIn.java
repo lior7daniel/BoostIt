6b24caf02e717bf62f1caea271c71f2d4c5bf698
liordaniel <lior7daniel@gmail.com>
2019-12-12 01:47:35 +0200

    LogIn Design - Done! Intents to registration for new couch \ trainee.

---
+++ app/src/main/java/com/example/boostit/LogIn.java
@@ -0,0 +1,32 @@
+package com.example.boostit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        findViewById(R.id.btnNewCouch).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentNewCouch = new Intent(LogIn.this, RegisterCouch.class);
+                startActivity(intentNewCouch);
+            }
+        });
+
+        findViewById(R.id.btnNewTrainee).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentNewTrainee = new Intent(LogIn.this, RegisterTrainee.class);
+                startActivity(intentNewTrainee);
+            }
+        });
+    }
+}
