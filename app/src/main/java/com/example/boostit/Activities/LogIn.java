b67a822aff7edf8722f0d118c80ab9c42a712059
Amir Adar <44618095+amiradar12345@users.noreply.github.com>
2019-12-21 16:40:06 +0200

    int amir

--- app/src/main/java/com/example/boostit/Activities/LogIn.java
+++ app/src/main/java/com/example/boostit/Activities/LogIn.java
@@ -23,7 +23,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class  LogIn extends AppCompatActivity {
-
+    int amir = 100;
     String                          uId;
     FirebaseAuth                    myAuth;
     EditText                        txtEmail, txtPassword;
